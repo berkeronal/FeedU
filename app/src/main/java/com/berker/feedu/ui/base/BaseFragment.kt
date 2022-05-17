a7ef9e6fb5e603c8a32d370cb1143c9fb371c452
Berker <berker435@gmail.com>
2022-05-18 01:57:05 +0300

    Base Fragment and missing dependencies added

---
+++ app/src/main/java/com/berker/feedu/ui/base/BaseFragment.kt
@@ -0,0 +1,45 @@
+package com.berker.feedu.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    abstract fun initUi()
+
+    @LayoutRes
+    abstract fun layoutId(): Int
+
+    open fun initOnCreateView(savedInstanceState: Bundle?) {}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId(), container, false)
+        initOnCreateView(savedInstanceState)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initUi()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/berker/feedu/ui/main/FeedListFragment.kt
@@ -0,0 +1,14 @@
+package com.berker.feedu.ui.main
+
+import com.berker.feedu.R
+import com.berker.feedu.databinding.FragmentFeedListBinding
+import com.berker.feedu.ui.base.BaseFragment
+
+class FeedListFragment : BaseFragment<FragmentFeedListBinding>() {
+
+    override fun initUi() {
+
+    }
+
+    override fun layoutId() = R.layout.fragment_feed_list
+}
